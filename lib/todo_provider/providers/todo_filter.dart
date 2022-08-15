8b51c9e504a32bf9690230aae5ee9c4cd948dc4d
Mies <nature9th@gmail.com>
2022-08-15 14:02:44 +0900

    study: TodoFilterState and TodoFilter ChangeNotifier
    - udemy 'Flutter Provider Essential 코스' by Sangwook Cho

---
+++ lib/todo_provider/providers/todo_filter.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cheat_repo/todo_provider/models/todo_model.dart';
+
+class TodoFilterState extends Equatable {
+  final Filter filter;
+
+  TodoFilterState({
+    required this.filter,
+  });
+
+  factory TodoFilterState.initial() {
+    return TodoFilterState(filter: Filter.all);
+  }
+
+  @override
+  List<Object> get props => [filter];
+
+  @override
+  bool get stringify => true;
+
+  TodoFilterState copyWith({
+    Filter? filter,
+  }) {
+    return TodoFilterState(
+      filter: filter ?? this.filter,
+    );
+  }
+}
+
+class TodoFilter with ChangeNotifier {
+  TodoFilterState _state = TodoFilterState.initial();
+
+  TodoFilterState get state => _state;
+
+  void changeFilter(Filter newFilter) {
+    _state = _state.copyWith(filter: newFilter);
+    notifyListeners();
+  }
+}
