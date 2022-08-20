a868d1046c8d10359bb274f2f31ff8703231c323
Mies <nature9th@gmail.com>
2022-08-20 22:07:07 +0900

    study: ProxyProvider로 리팩터링
    - udemy 'Flutter Provider Essential 코스' by Sangwook Cho

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cheat_repo/todo_provider/todo_app.dart';
+import 'package:flutter_cheat_repo/todo_provider_refactoring/todo_app_refactoring.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,6 +7,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   /// 프로바이더를 활용한 To-do app
+  // @override
+  // Widget build(BuildContext _) => TodoApp();
+
+  /// 프로바이더를 활용한 To-do app
+  /// ChangeNotifierProxyProvider를 ProxyProvider로 리팩터링
   @override
-  Widget build(BuildContext _) => TodoApp();
+  Widget build(BuildContext _) => TodoAppRefactoring();
 }
---
+++ lib/todo_provider_refactoring/todo_app_refactoring.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cheat_repo/todo_provider_refactoring/pages/todos_page.dart';
+import 'package:flutter_cheat_repo/todo_provider_refactoring/providers/index.dart';
+import 'package:provider/provider.dart';
+
+class TodoAppRefactoring extends StatelessWidget {
+  const TodoAppRefactoring({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<TodoFilter>(
+          create: (_) => TodoFilter(),
+        ),
+        ChangeNotifierProvider<TodoSearch>(
+          create: (_) => TodoSearch(),
+        ),
+        ChangeNotifierProvider<TodoList>(
+          create: (_) => TodoList(),
+        ),
+        ProxyProvider<TodoList, ActiveTodoCount>(
+          update: (
+            BuildContext _,
+            TodoList todoList,
+            ActiveTodoCount? __,
+          ) =>
+              ActiveTodoCount(todoList: todoList),
+        ),
+        ProxyProvider3<TodoFilter, TodoSearch, TodoList, FilteredTodos>(
+          update: (
+            BuildContext _,
+            TodoFilter todoFilter,
+            TodoSearch todoSearch,
+            TodoList todoList,
+            FilteredTodos? __,
+          ) =>
+              FilteredTodos(
+            todoFilter: todoFilter,
+            todoSearch: todoSearch,
+            todoList: todoList,
+          ),
+        )
+      ],
+      child: MaterialApp(
+        title: 'TODOS',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        home: TodosPage(),
+      ),
+    );
+  }
+}
